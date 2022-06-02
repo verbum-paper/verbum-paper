e58322ba032a748c63c72485869dd28cc2059730
melchisedech333 <melchisedech333@gmail.com>
2022-06-02 18:22:55 -0300

    Updates

---
+++ verbum-install/verbum-install.sh
@@ -0,0 +1,37 @@
+
+# Clone repository.
+rm -rf verbum-paper
+git clone https://github.com/verbum-paper/verbum-paper.git
+
+# Prepare directories.
+cd verbum-paper
+mv dependencies verbum-paper/libs
+cd verbum-paper/libs
+
+# Extract files.
+unzip "ace-v1.5.3.zip"
+unzip "chart.js-v3.8.0.zip"
+unzip "d3-v7.4.4.zip"
+unzip "function-plot-v1.22.8.zip"
+unzip "highlightjs-v11.5.1.zip"
+unzip "html2canvas-v1.4.1.zip"
+unzip "jquery-v3.6.0.zip"
+unzip "mathjax-v3.2.1.zip"
+unzip "mathlive-v0.73.7.zip"
+unzip "polyfill-v8.2.1.zip"
+
+# Clean files.
+rm -rf *.zip
+
+# Download electron.
+cd ../..
+mkdir "electron-v18.3.0"
+cd "electron-v18.3.0"
+wget -c "https://github.com/electron/electron/releases/download/v18.3.0/electron-v18.3.0-linux-x64.zip"
+unzip "electron-v18.3.0-linux-x64.zip"
+rm -rf "electron-v18.3.0-linux-x64.zip"
+cd ..
+
+echo "Finished <3"
+
+
