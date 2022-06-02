702a8e949a79fca14b690ff4e7d5b97b4fa9da28
melchisedech333 <melchisedech333@gmail.com>
2022-06-02 19:26:35 -0300

    Updates

---
+++ verbum-paper/js/verbum-paper.js
@@ -0,0 +1,46 @@
+
+/*
+** Jesus <3
+**
+** Main script.
+*/
+
+$(document).ready(() => {
+
+    console.log('Verbum Paper -> Started!')
+
+})
+
+/*
+** Resize areas control.
+*/
+
+var mousePressed = false
+
+$(document).ready(() => {
+
+    $('.separator-area').mousedown(() => {
+        mousePressed = true
+    })
+
+    $('.separator-area').mouseup(() => {
+        mousePressed = false
+    })
+
+    $(window).mouseup(() => {
+        mousePressed = false
+    })
+
+    $(window).mousemove((e) => {
+        if (mousePressed) {
+            var left = e.clientX;
+            
+            $('.code-area').css('width', left +'px')
+            $('.separator-area').css('left', left +'px')
+            $('.draw-area').css('width', 'calc( ( 100% - '+ left +'px ) - 5px )')
+        }
+    })
+
+})
+
+
