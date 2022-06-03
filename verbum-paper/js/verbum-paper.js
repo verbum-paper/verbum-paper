8cbaa6b826827524b00b105fb5a832736d0a4d86
melchisedech333 <melchisedech333@gmail.com>
2022-06-02 21:03:12 -0300

    Updates

--- verbum-paper/js/verbum-paper.js
+++ verbum-paper/js/verbum-paper.js
@@ -53,9 +53,77 @@ $(document).ready(() => {
 
     codeEditor = ace.edit("code-editor")
     codeEditor.setTheme("ace/theme/twilight")
-    codeEditor.session.setMode("ace/mode/javascript")
+    codeEditor.session.setMode("ace/mode/html")
     codeEditor.setAutoScrollEditorIntoView(true)
 
+})
+
+/*
+** Tests...
+*/
+
+$(document).ready(() => {
+
+    codeEditor.setValue(`
+    
+<p>$$e^{i\pi}$$</p>
+
+<p>
+\[x = {-b \pm \sqrt{b^2-4ac} \over 2a}.\]
+</p>
+
+<p>
+    LaTex example:<br>
+
+    \[x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}\]
+    $$x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}$$
+</p>
+
+
+<math-field virtual-keyboard-mode="manual">
+    x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}
+</math-field>
+
+
+<pre><code class="language-c">// Jesus eh Top xD
+
+int main (int argc, char *argv[])
+{
+    int numero1 = 2;
+    int numero2 = 3;
+
+    if (numero1 == 2 || numero2 == 2)
+    {
+        printf("Expressao 2\n");
+    }
+
+    else if ( numero1 == 2 && numero2 == 3 )
+    {
+        printf("Expressao 1\n");
+    }
+
+    return 0;
+} </code></pre>
+
+
+<p>$$e^{i\pi}$$</p>
+
+<p>
+\[x = {-b \pm \sqrt{b^2-4ac} \over 2a}.\]
+</p>
+
+<p>
+    LaTex example:<br>
+
+    \[x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}\]
+    $$x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}$$
+</p>
+
+
+<math-field virtual-keyboard-mode="manual">
+    x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}
+</math-field>
+    `, -1)
 })
 
 
